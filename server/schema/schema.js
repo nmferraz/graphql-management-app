82bcd67168a65838fc09f69f3f726661458af578
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-07-09 22:01:39 +0100

    Project Type & Queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,6 +10,17 @@ const {
   GraphQLEnumType,
 } = require("graphql");
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 // Client Type
 const ClientType = new GraphQLObjectType({
   name: "Client",
@@ -24,6 +35,19 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
