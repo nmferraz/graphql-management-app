e07b99dac380103f3a9e931fe6b5dbfc592a1dc1
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-07-10 09:38:21 +0100

    database connection

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const colors = require("colors");
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(
   "/graphql",
